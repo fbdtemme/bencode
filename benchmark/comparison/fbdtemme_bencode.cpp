05b69d4c74db960bb8679e30826f4743142ad920
fbdtemme <floriandetemmerman@gmail.com>
2020-12-06 16:38:01 +0100

    Add comparison to jimporter/bencode benchmark

---
+++ benchmark/comparison/fbdtemme_bencode.cpp
@@ -0,0 +1,50 @@
+
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+#include <catch2/catch.hpp>
+#include <chrono>
+
+#include <ranges>
+#include <string_view>
+#include <fstream>
+
+#include "bencode/bencode.hpp"
+
+using namespace std::string_view_literals;
+
+namespace fs = std::filesystem;
+
+inline void benchmark_helper(std::istream& ifs)
+{
+    std::string torrent(
+            std::istreambuf_iterator<char>{ifs},
+            std::istreambuf_iterator<char>{});
+
+    BENCHMARK("decode_value") {
+        auto d = bencode::decode_value(torrent);
+        return d;
+    };
+
+    BENCHMARK("decode_view") {
+         auto d = bencode::decode_view(torrent);
+         return d;
+     };
+}
+
+
+TEST_CASE("benchmark parsing - fbdtemme/bencode")
+{
+    SECTION("fedora workstation") {
+        std::ifstream ifs(RESOURCES_DIR"/Fedora-Workstation-Live-x86_64-30.torrent");
+        benchmark_helper(ifs);
+    }
+
+    SECTION("NASA mdim_color") {
+        std::ifstream ifs(RESOURCES_DIR"/NASA-Viking-Merged-Color-Mosaic.torrent");
+        benchmark_helper(ifs);
+    }
+
+    SECTION("COVID-19 image dataset") {
+        std::ifstream ifs(RESOURCES_DIR"/COVID-19-image-dataset-collection.torrent");
+        benchmark_helper(ifs);
+    }
+}
---
+++ benchmark/comparison/main.cpp
@@ -0,0 +1,4 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+
+#include <catch2/catch.hpp>
