c33b5aca554efa5349d3bd8a636c108f9c45dcdd
fbdtemme <floriandetemmerman@gmail.com>
2020-12-26 13:44:20 +0100

    Fix libtorrent package name

--- external/libtorrent.cmake
+++ external/libtorrent.cmake
@@ -1,15 +1,16 @@
 include(FetchContent)
 
-find_package(libtorrent QUIET)
-if (libtorrent_FOUND)
-    message(STATUS "Local installation of libtorrent found.")
+find_package(LibtorrentRasterbar QUIET)
+if (LibtorrentRasterbar_FOUND)
+    message(STATUS "Local installation of libtorrent-rasterbar found.")
 else()
-    message(STATUS "Fetching dependency libtorrent...")
+    message(STATUS "Fetching dependency libtorrent-rasterbar...")
     FetchContent_Declare(
-            libtorrent
+            libtorrent-rasterbar
             GIT_REPOSITORY https://github.com/arvidn/libtorrent.git
             GIT_TAG        RC_2_0
+            EXCLUDE_FROM_ALL TRUE
     )
-    FetchContent_MakeAvailable(libtorrent)
+    FetchContent_MakeAvailable(libtorrent-rasterbar)
     add_library(LibtorrentRasterbar::torrent-rasterbar ALIAS torrent-rasterbar)
 endif()
